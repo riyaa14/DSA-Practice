3969e836a9fde5aef095f2beb8bfc37898f53643
Riya Garg <96983874+riyaa14@users.noreply.github.com>
2022-11-02 00:53:22 +0530

    Time: 58 ms (43.94%), Space: 13.2 MB (52.18%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> findBall(vector<vector<int>>& grid) {
+        
+        vector<int> ans;
+        int i=0, j;
+        for(int k=0; k<grid[0].size(); k++){
+            i=0;
+            j = k;
+            while(i<grid.size()){
+                
+                // corner cases
+                if((j==grid[0].size()-1 and grid[i][j] == 1) or (j==0 and grid[i][j] == -1)) break;
+                if(grid[i][j] == 1 and grid[i][j+1] == -1 or grid[i][j] == -1 and grid[i][j-1] == 1) break;
+                
+                if(grid[i][j] == 1){
+                    i++; j++;
+                }else if(grid[i][j] == -1){
+                    i++; j--;
+                }
+                
+            }
+            
+            if(i<grid.size()) ans.push_back(-1);
+            else ans.push_back(j);
+            
+        }
+        
+        return ans;
+        
+    }
+};
