cff36a713ebe047b914736b9fd66c52d2fb996b3
Riya Garg <96983874+riyaa14@users.noreply.github.com>
2023-01-30 23:18:36 +0530

    Added solution - LeetHub

---
+++ Reverse a Stack - GFG/reverse-a-stack.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    
+    void insertAtBottom(int x, stack<int>& St){
+        if(St.empty()){
+            St.push(x);
+            return;
+        }
+        
+        int topElem = St.top();
+        St.pop();
+        insertAtBottom(x, St);
+        St.push(topElem);
+        
+        return;
+    }
+    
+    void Reverse(stack<int> &St){
+        if(St.empty()) return;
+        
+        int topElem = St.top();
+        St.pop();
+        Reverse(St);
+        insertAtBottom(topElem, St);
+        return;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int N;
+        cin>>N;
+        stack<int> St;
+        for(int i=0;i<N;i++){
+            int x;
+            cin>>x;
+            St.push(x);
+        }
+        Solution ob;
+        ob.Reverse(St);
+        vector<int>res;
+        while(St.size())
+        {
+            res.push_back(St.top());
+            St.pop();
+        }
+        for(int i = res.size()-1;i>=0;i--)
+        {
+            cout<<res[i]<<" ";
+        }
+        
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
